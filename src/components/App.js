aa846bacf29a99469114a69630a29ce526ffde70
brianmuigai2-stack <brian.muigai2@student.moringaschool.com>
2025-10-15 16:14:33 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React from 'react'
+import { useEffect, useState } from 'react'
+const APIURL = "https://dog.ceo/api/breeds/image/random"
+
+function App() {
+    const [image, setImage] = useState(null);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((res) => res.json())
+        .then((data) => setImage(data.message))
+    }, [])
+
+    if(!image) {
+        return <p>Loading...</p>
+    }
+  return (
+    <div>
+        
+        <img alt="A Random Dog" src={image}/>
+
+    </div>
+  )
+}
+
+export default App
